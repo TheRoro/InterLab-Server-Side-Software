4c52cf7a58b534d85fa00d95f5f8d9e0525aa1cf
omar2702 <34077377+omar2702@users.noreply.github.com>
2020-04-26 19:20:11 -0500

    Version 2
    Añadido todas las clases del diagrama de base datos y relación de User (uno) a Student (mas)

---
+++ InterLab/Domain/Models/Internship.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterLab.Domain.Models
+{
+    public class Internship
+    {
+
+        public int Id { get; set; }
+        
+        public string State { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DatePublished { get; set; } //Set is only used on creation ask Angel
+
+        public DateTime StartingDate { get; set; }
+
+        public DateTime FinishingDate { get; set; }
+
+        public double Salary { get; set; }
+
+        public int Participants { get; set; }
+
+        //Relation
+        public int EntrepreneurId { get; set; }
+        public Entrepreneur Entrepreneur {get; set;}
+
+        public IList<Request> Requests { get; set; } = new List<Request>();
+    }
+}
