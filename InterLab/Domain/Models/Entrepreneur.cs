7d5ccf45e6d1212eeb9659d0e67edab8721007bd
omar2702 <34077377+omar2702@users.noreply.github.com>
2020-04-26 19:20:11 -0500

    Version 2
    Añadido todas las clases del diagrama de base datos y relación de User (uno) a Student (mas)

---
+++ InterLab/Domain/Models/Entrepreneur.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterLab.Domain.Models
+ 
+{
+    public class Entrepreneur
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Id_RUC { get; set; }
+
+        //RelationShips
+
+        public IList<Internship> Internships { get; set; } = new List<Internship>();
+    }
+}
