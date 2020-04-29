9f6672e83c87b69e9d079e78fc588fa3705a106a
Omar Alonso Clavijo Mestas <u201817916@upc.edu.pe>
2020-04-28 21:21:21 -0500

    Añadido todas las clases del diagrama de clases, movi el archivo Persistence afuera de la carpeta Domain.
    Nota:
    
    En la carpeta Domain -> Model, van los atributos de la clase
    En la carpeta Domain -> Repository, van los metodos de las clases, crearlas como interface

--- InterLab/Domain/Models/Role.cs
+++ InterLab/Domain/Models/Role.cs
@@ -1,18 +1,21 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace InterLab.Domain.Models
 {
-    public class University
+    public class Role
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
 
-        public string Country { get; set; }
+        public string Type { get; set; }
 
+        public string Description { get; set; }
 
-        public IList<Student> Students { get; set; } = new List<Student>();
+        //Relationships
+
     }
 }
