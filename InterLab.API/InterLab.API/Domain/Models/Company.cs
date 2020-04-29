67a883b8ce73390fe4266519c1283d87509735e1
Omar Alonso Clavijo Mestas <u201817916@upc.edu.pe>
2020-04-29 17:45:31 -0500

    Creado InterLab.API con todas las clases y carpetas creadas en InterLab, eliminar el archivo InterLab.

---
+++ InterLab.API/InterLab.API/Domain/Models/Company.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterLab.Domain.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Sector { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Phone_Number { get; set; }
+
+        //Puede ser servicio google maps
+        public string Address { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        //Relation Qualification
+        public IList<Qualification> Qualifications { get; set; } = new List<Qualification>();
+    }
+}
---
+++ InterLab.API/InterLab.API/Domain/Models/Role.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterLab.Domain.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string Description { get; set; }
+
+        //Relationships
+
+    }
+}
