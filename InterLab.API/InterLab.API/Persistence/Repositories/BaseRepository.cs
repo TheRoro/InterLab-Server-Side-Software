1eebf95591b6b490ab18b649e4588f481bced184
Omar Alonso Clavijo Mestas <u201817916@upc.edu.pe>
2020-05-01 18:03:39 -0500

    Creada la clase BaseRepository

---
+++ InterLab.API/InterLab.API/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using InterLab.API.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterLab.API.Persistence.Repositories
+{
+
+    public abstract class BaseRepository
+    {
+
+        protected readonly AppDbContext _context;
+
+        public BaseRepository(AppDbContext context)
+            {
+            _context = context;
+            }
+    }
+}
