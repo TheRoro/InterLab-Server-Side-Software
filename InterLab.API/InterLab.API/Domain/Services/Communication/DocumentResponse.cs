3d656484e9e1d216edca160a94fe9150d78e56ec
Juanca0312 <39997847+Juanca0312@users.noreply.github.com>
2020-05-21 16:37:33 -0500

    Agregada IDocumentService e implementacion de Repository

--- InterLab.API/InterLab.API/Domain/Services/Communication/DocumentResponse.cs
+++ InterLab.API/InterLab.API/Domain/Services/Communication/DocumentResponse.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using InterLab.API.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace InterLab.API.Domain.Services.Communication
 {
-    public class DocumentResponse
+    public class DocumentResponse : BaseResponse<Document>
     {
+        public DocumentResponse(Document document) : base(document) { }
+        public DocumentResponse(string message) : base(message) { }
     }
 }
