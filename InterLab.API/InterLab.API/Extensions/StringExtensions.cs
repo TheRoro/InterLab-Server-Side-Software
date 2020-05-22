3f0dc484c2537129f60c4581bbc2e9b5c539128c
Omar Alonso Clavijo Mestas <u201817916@upc.edu.pe>
2020-05-22 14:20:17 -0500

    Creado la carpeta Extensions con sus clases

---
+++ InterLab.API/InterLab.API/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterLab.API.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToSnakeCase(this string str)
+        {
+            return string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToLower();
+        }
+    }
+}
