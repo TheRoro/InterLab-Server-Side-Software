3c610173d2352cd530e9b05035e08a9f3582a9cb
Omar Alonso Clavijo Mestas <u201817916@upc.edu.pe>
2020-05-21 20:44:25 -0500

    Añadido las clases para los endpoint de Profile (Services, Repository, Controller)

--- InterLab.API/InterLab.API/Domain/Services/IProfileService.cs
+++ InterLab.API/InterLab.API/Domain/Services/IProfileService.cs
@@ -1,3 +1,5 @@
+using InterLab.API.Domain.Models;
+using InterLab.API.Domain.Services.Communication;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,5 +9,29 @@ namespace InterLab.API.Domain.Services
 {
     public interface IProfileService
     {
+        //get All
+        Task<IEnumerable<Profile>> ListAsync();
+
+        ////get By StudentId
+        //Task<IEnumerable<Profile>> ListByStudentId(int studentId);
+
+        ////get By WorkerId
+        //Task<IEnumerable<Profile>> ListByWorkerId(int workerId); 
+
+
+        //getByStudentId and ProfileId
+        Task<Profile> GetByIdAndStudentId(int id, int studentId);
+
+
+        //getByWorkerId and ProfileId
+        Task<Profile> GetByIdAndWorkerId(int id, int workerId);
+
+
+        //gett by FindId
+        Task<ProfileResponse> GetByIdAsync(int id);
+
+        Task<ProfileResponse> SaveAsync(Profile profile); 
+        Task<ProfileResponse> UpdateAsync(int id, Profile profile);
+        Task<ProfileResponse> DeleteAsync(int id);
     }
 }
