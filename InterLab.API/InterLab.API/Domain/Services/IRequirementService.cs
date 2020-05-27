11c9c9bbf4c95b340bff69ebf9eac4743baf360e
Rodrigo Ramírez <8rb.academic@gmail.com>
2020-05-27 18:15:08 -0500

    Finished the repositories, responses and services of the entity Requirement.

---
+++ InterLab.API/InterLab.API/Domain/Services/IRequirementService.cs
@@ -0,0 +1,19 @@
+﻿using InterLab.API.Domain.Models;
+using InterLab.API.Domain.Services.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterLab.API.Domain.Services
+{
+    public interface IRequirementService
+    {
+        Task<IEnumerable<Requirement>> ListAsync();
+        Task<IEnumerable<Requirement>> ListByInternshipIdAsync(int internshipId);
+        Task<RequirementResponse> GetByIdAsync(int id);
+        Task<RequirementResponse> SaveAsync(Requirement requirement);
+        Task<RequirementResponse> UpdateAsync(int id, Requirement requirement);
+        Task<RequirementResponse> DeleteAsync(int id);
+    }
+}
