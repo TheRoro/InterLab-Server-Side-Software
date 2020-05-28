830d360b5401f1716e4514c0a32c1eae839d0628
Rodrigo Ramírez <8rb.academic@gmail.com>
2020-05-27 19:31:40 -0500

    Finished controller for requirement

---
+++ InterLab.API/InterLab.API/Controllers/RequirementsController.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using InterLab.API.Domain.Models;
+using InterLab.API.Domain.Services;
+using InterLab.API.Extensions;
+using InterLab.API.Resources;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterLab.API.Controllers
+{
+    [Route("/api/[controller]")]
+    public class RequirementsController : Controller
+    {
+        private readonly IRequirementService _requirementService;
+        private readonly IMapper _mapper;
+
+        public RequirementsController(IRequirementService requirementService, IMapper mapper)
+        {
+            _requirementService = requirementService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<RequirementResource>> GetAllAsync()
+        {
+            var requirements = await _requirementService.ListAsync();
+            var resources = _mapper
+                .Map<IEnumerable<Requirement>, IEnumerable<RequirementResource>>(requirements);
+            return resources;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var result = await _requirementService.GetByIdAsync(id);
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var requirementResource = _mapper.Map<Requirement, RequirementResource>(result.Resource);
+            return Ok(requirementResource);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] SaveRequirementResource resource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var requirement = _mapper.Map<SaveRequirementResource, Requirement>(resource);
+            var result = await _requirementService.SaveAsync(requirement);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            var requirementResource = _mapper.Map<Requirement, RequirementResource>(result.Resource);
+            return Ok(requirementResource);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAsync(int id, [FromBody] SaveRequirementResource resource)
+        {
+            var requirement = _mapper.Map<SaveRequirementResource, Requirement>(resource);
+            var result = await _requirementService.UpdateAsync(id, requirement);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var requirementResource = _mapper.Map<Requirement, RequirementResource>(result.Resource);
+            return Ok(requirementResource);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var result = await _requirementService.DeleteAsync(id);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var requirementResource = _mapper.Map<Requirement, RequirementResource>(result.Resource);
+            return Ok(requirementResource);
+        }
+    }
+}
