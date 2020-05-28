3818621bfa528d255ea42c74ba2e0df960655580
Rodrigo Ramírez <8rb.academic@gmail.com>
2020-05-27 19:31:40 -0500

    Finished controller for requirement

---
+++ InterLab.API/InterLab.API/Controllers/InternshipRequirementsController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using InterLab.API.Domain.Models;
+using InterLab.API.Domain.Services;
+using InterLab.API.Extensions;
+using InterLab.API.Resources;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterLab.API.Controllers
+{
+    [Route("/api/internships/{internshipId}/requirements")]
+    public class InternshipRequirementsController : Controller
+    {
+        private readonly IRequirementService _requirementService;
+        private readonly IMapper _mapper;
+
+        public InternshipRequirementsController(IRequirementService requirementService, IMapper mapper)
+        {
+            _requirementService = requirementService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<RequirementResource>> GetAllByInternshipIdAsync(int internshipId)
+        {
+            var requirements = await _requirementService.ListByInternshipIdAsync(internshipId);
+            var resources = _mapper
+                .Map<IEnumerable<Requirement>, IEnumerable<RequirementResource>>(requirements);
+            return resources;
+        }
+    }
+}
