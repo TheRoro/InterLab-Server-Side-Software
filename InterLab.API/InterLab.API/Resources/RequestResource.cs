132df7a1da41c26d64102e0b3b0902f9488d1898
Rodrigo Ramírez <8rb.academic@gmail.com>
2020-05-27 23:02:05 -0500

    Finished endpoints for entity request

---
+++ InterLab.API/InterLab.API/Resources/RequestResource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterLab.API.Resources
+{
+    public class RequestResource
+    {
+        public int Id { get; set; }
+        public string State { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ InterLab.API/InterLab.API/Resources/SaveRequestResource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterLab.API.Resources
+{
+    public class SaveRequestResource
+    {
+        [Required]
+        [MaxLength(20)]
+        public string State { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public DateTime CreationDate { get; set; }
+    }
+}
