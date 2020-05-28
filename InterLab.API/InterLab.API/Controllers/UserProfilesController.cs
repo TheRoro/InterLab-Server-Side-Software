eeaac8592d8a8d5bd915bda3806be8bdeba834e6
Omar Alonso Clavijo Mestas <u201817916@upc.edu.pe>
2020-05-28 13:51:36 -0500

    Añadido el endpoint /api/users/{userId}/profiles  en UserProfileController

---
+++ InterLab.API/InterLab.API/Controllers/UserProfilesController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using InterLab.API.Domain.Services;
+using InterLab.API.Resources;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterLab.API.Controllers
+{
+
+    [Route("/api/users/{userId}/profiles")] 
+    public class UserProfilesController : Controller
+    { 
+        private readonly IProfileService _profileService;
+        private readonly IMapper _mapper;
+
+
+        public UserProfilesController(IProfileService profileService, IMapper mapper)
+        {
+            _profileService = profileService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProfileResource>> GetByUserId(int userId)   
+        {
+            var profiles = await _profileService.ListByUserId(userId);
+            var resources = _mapper
+                .Map<IEnumerable<Domain.Models.Profile>, IEnumerable<ProfileResource>>(profiles);
+            return resources;
+        }
+
+    }
+}
